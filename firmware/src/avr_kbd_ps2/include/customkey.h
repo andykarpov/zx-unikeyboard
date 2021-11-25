6874b2fdb8f972807ae5fb7b0d763257338af70c
Andy Karpov <andy.karpov@gmail.com>
2021-11-25 11:11:36 +0200

    fixed scan

--- firmware/src/avr_kbd_ps2/include/customkey.h
+++ firmware/src/avr_kbd_ps2/include/customkey.h
@@ -77,11 +77,97 @@
 #define KEY_DOWN 76
 #define KEY_RIGHT 77
 
-const uint8_t keyaddr[8][3];
+const uint8_t keyaddr[][3] =
+{
+{ KEY_ESC, 5, 26 },
+{ KEY_F1, 2, 25 },
+{ KEY_F2, 2, 24 },
+{ KEY_F3, 7, 24 },
+{ KEY_F4, 5, 24 },
+{ KEY_F5, 1, 11 },
+{ KEY_F6, 5, 21 },
+{ KEY_F7, 7, 20 },
+{ KEY_F8, 2, 20 },
+{ KEY_F9, 2, 14 },
+{ KEY_F10, 1, 14 },
+{ KEY_F11, 4, 16 },
+{ KEY_F12, 8, 11 },
+{ KEY_DELETE, 2, 16 },
+{ KEY_ACCENT, 2, 26 },
+{ KEY_1, 1, 26 },
+{ KEY_2, 1, 25 },
+{ KEY_3, 1, 24 },
+{ KEY_4, 1, 23 },
+{ KEY_5, 2, 23 },
+{ KEY_6, 2, 22 },
+{ KEY_7, 1, 22 },
+{ KEY_8, 1, 21 },
+{ KEY_9, 1, 20 },
+{ KEY_0, 1, 13 },
+{ KEY_MINUS, 2, 13 },
+{ KEY_EQUAL, 2, 21 },
+{ KEY_BACKSPACE, 7, 14 },
+{ KEY_TAB, 7, 26 },
+{ KEY_Q, 8, 26 },
+{ KEY_W, 8, 25 },
+{ KEY_E, 8, 24 },
+{ KEY_R, 8, 23 },
+{ KEY_T, 7, 23 },
+{ KEY_Y, 7, 22 },
+{ KEY_U, 8, 22 },
+{ KEY_I, 8, 21 },
+{ KEY_O, 8, 20 },
+{ KEY_P, 8, 13 },
+{ KEY_LBRACKET, 7, 13 },
+{ KEY_RBRACKET, 7, 21 },
+{ KEY_BACKSLASH, 6, 14 },
+{ KEY_CAPSLOCK, 7, 25 },
+{ KEY_A, 6, 26 },
+{ KEY_S, 6, 25 },
+{ KEY_D, 6, 24 },
+{ KEY_F, 6, 23 },
+{ KEY_G, 5, 23 },
+{ KEY_H, 5, 22 },
+{ KEY_J, 6, 22 },
+{ KEY_K, 6, 21 },
+{ KEY_L, 6, 20 },
+{ KEY_SEMICOLON, 6, 13 },
+{ KEY_QUOTE, 5, 13 },
+{ KEY_ENTER, 4, 14 },
+{ KEY_LSHIFT, 7, 15 },
+{ KEY_Z, 4, 26 },
+{ KEY_X, 4, 25 },
+{ KEY_C, 4, 24 },
+{ KEY_V, 4, 23 },
+{ KEY_B, 3, 23 },
+{ KEY_N, 3, 22 },
+{ KEY_M, 4, 22 },
+{ KEY_COMMA, 4, 21 },
+{ KEY_PERIOD, 4, 20 },
+{ KEY_SLASH, 3, 13 },
+{ KEY_RSHIFT, 6, 15 },
+{ KEY_LCTRL, 2, 11 },
+{ KEY_FN, 6, 12 },
+{ KEY_GUI, 7, 10 },
+{ KEY_LALT, 5, 12 },
+{ KEY_SPACE, 5, 16 },
+{ KEY_RALT, 3, 12 },
+{ KEY_RCTRL, 4, 11 },
+{ KEY_LEFT, 3, 19 },
+{ KEY_UP, 5, 19 },
+{ KEY_DOWN, 3, 16 },
+{ KEY_RIGHT, 3, 17 },
+};
 
-#define COLS_MAX 1
-#define ROWS_MAX 1
+#define COLS_MAX 8
+#define ROWS_MAX 16
 
-const uint8_t rows[ROWS_MAX];
+const uint8_t cols[COLS_MAX] = 
+{
+1, 2, 3, 4, 5, 6, 7, 8
+};
 
-const uint8_t cols[COLS_MAX];
+const uint8_t rows[ROWS_MAX] = 
+{
+10, 11, 12, 13, 14, 15, 16, 17, 19, 20, 21, 22, 23, 24, 25, 26
+};
